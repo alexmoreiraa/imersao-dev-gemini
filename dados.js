f65064cd89b6afa07086a605c3bce1e11f2dab3d
alexmoreiraa <alexjuniorsilvamoreira@gmail.com>
2024-09-10 17:09:29 -0300

    first commit

---
+++ dados.js
@@ -0,0 +1,26 @@
+let dados = [
+    {
+    titulo : "Rayssa Leal",
+    descricao : "Rayssa Leal, a fenômeno do skate, conquistou o mundo com sua habilidade e carisma. Aos poucos anos, ela se tornou a mais jovem medalhista olímpica da história do Brasil, conquistando o bronze nos Jogos Olímpicos de Tóquio. Sua trajetória inspiradora, marcada por superação e paixão pelo skate, a tornou um ícone para jovens skatistas e um orgulho nacional. Com seu estilo único e personalidade contagiante, Rayssa continua a revolucionar o esporte e a inspirar milhões de pessoas ao redor do mundo.",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags : "Fadinha",
+},
+{
+    titulo : "Rebeca Andrade",
+    descricao : "Rebeca Andrade, a joia da ginástica brasileira, tem conquistado o mundo com sua força, talento e determinação. Bicampeã olímpica, ela é a maior medalhista da história do Brasil nos Jogos Olímpicos, com um currículo invejável que inclui medalhas de ouro, prata e bronze. Sua trajetória inspiradora, marcada por superação e dedicação, a tornou um símbolo nacional e um exemplo para jovens atletas. Com sua habilidade única e carisma contagiante, Rebeca continua a fazer história na ginástica mundial e a encher de orgulho todos os brasileiros.",
+    link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags : "ouro"
+},
+{
+    titulo : "Beatriz Souza",
+    descricao : "Beatriz Souza, a joia do judô brasileiro, tem se destacado no cenário internacional com suas técnicas impecáveis e determinação. Com sua força e agilidade, ela conquistou diversas medalhas em competições importantes, demonstrando seu grande potencial. Sua trajetória promissora a coloca como uma das principais apostas para representar o Brasil em futuras competições internacionais.",
+    link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags : "ouro" 
+}
+    ];
+
+/*
+Autor: Alex Moreira
+projeto: Imersão Alura Dev com Google Gemini
+data: 10/09/2024 
+*/
